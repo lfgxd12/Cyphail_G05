875b801815a8d63e82f70cf9fe0f31b6d68982cd
Angel  Rojas Ruano <129477757+NoSoyANG3L@users.noreply.github.com>
2026-08-30 22:37:50 -0600

    Add documentation header to HelpHandler class
    Added documentation header with project details and authors.

--- src/main/java/cr/ac/una/eif400/cyphail/frontend/handlers/HelpHandler.java
+++ src/main/java/cr/ac/una/eif400/cyphail/frontend/handlers/HelpHandler.java
@@ -2,6 +2,13 @@ package cr.ac.una.eif400.cyphail.frontend.handlers;
 
 import cr.ac.una.eif400.cyphail.frontend.ReplCommand;
 
+/**
+ * Cyphail - Graph Query Engine Prototype
+ * EIF400-II-2026 - Escuela de Informatica, UNA
+ * Grupo: G05
+ * Autores: Luis Felipe Jimenez Fernandez, Jose David Chavarria Villalobos,
+ *          Jostin Jimenez Alfaro, Angel Rojas Ruano
+ */
 public class HelpHandler implements ReplCommand {
     @Override
     public void execute(String args) {
@@ -11,7 +18,7 @@ public class HelpHandler implements ReplCommand {
               .about            Show project authors and course details
               .use [graph]      List available graphs or select a graph
               .exit             Exit the REPL
-            
+
             Engine Queries:
               Type Cyphail statements directly (e.g., MATCH (p:Persona) RETURN p.nombre, p.edad)
             """);
