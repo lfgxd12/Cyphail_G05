d7f854a34ce511eeb34cbda623c11d2035583e86
Angel  Rojas Ruano <129477757+NoSoyANG3L@users.noreply.github.com>
2026-08-30 22:42:57 -0600

    Add project header comment to TablePrinter.java
    Added a header comment with project details and authors.

--- src/main/java/cr/ac/una/eif400/cyphail/output/TablePrinter.java
+++ src/main/java/cr/ac/una/eif400/cyphail/output/TablePrinter.java
@@ -2,6 +2,13 @@ package cr.ac.una.eif400.cyphail.output;
 
 import com.github.freva.asciitable.AsciiTable;
 
+/**
+ * Cyphail - Graph Query Engine Prototype
+ * EIF400-II-2026 - Escuela de Informatica, UNA
+ * Grupo: G05
+ * Autores: Luis Felipe Jimenez Fernandez, Jose David Chavarria Villalobos,
+ *          Jostin Jimenez Alfaro, Angel Rojas Ruano
+ */
 public class TablePrinter {
     public static void printTable(String[] headers, String[][] data) {
         System.out.println(AsciiTable.getTable(headers, data));
