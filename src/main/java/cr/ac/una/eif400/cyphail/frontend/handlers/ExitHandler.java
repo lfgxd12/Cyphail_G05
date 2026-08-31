a73786275a51e67d9364cc64e3ed38aa164bba28
Angel  Rojas Ruano <129477757+NoSoyANG3L@users.noreply.github.com>
2026-08-30 22:36:59 -0600

    Add project header comment to ExitHandler.java
    Added header comment with project details and authors.

--- src/main/java/cr/ac/una/eif400/cyphail/frontend/handlers/ExitHandler.java
+++ src/main/java/cr/ac/una/eif400/cyphail/frontend/handlers/ExitHandler.java
@@ -2,6 +2,13 @@ package cr.ac.una.eif400.cyphail.frontend.handlers;
 
 import cr.ac.una.eif400.cyphail.frontend.ReplCommand;
 
+/**
+ * Cyphail - Graph Query Engine Prototype
+ * EIF400-II-2026 - Escuela de Informatica, UNA
+ * Grupo: G05
+ * Autores: Luis Felipe Jimenez Fernandez, Jose David Chavarria Villalobos,
+ *          Jostin Jimenez Alfaro, Angel Rojas Ruano
+ */
 public class ExitHandler implements ReplCommand {
     @Override
     public void execute(String args) {
